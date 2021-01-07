e0bfb44554741f9b488ad63d19edfbb346c4c765
Justin <justin82049@gmail.com>
2021-01-06 20:21:39 -0500

    added the stub codes and instance variables

---
+++ MyLinkedList.java
@@ -0,0 +1,12 @@
+public class MyLinkedList{
+ private int size;
+ private Node start,end;  
+ public MyLinkedList(){/*create a constructor*/}
+ public int size();
+ public boolean add(String value);
+ public void add(int index, String value);
+ public String get(int index);
+ public String set(int index, String value);
+ public String toString();
+ //Any helper method that returns a Node object MUST BE PRIVATE!
+}
