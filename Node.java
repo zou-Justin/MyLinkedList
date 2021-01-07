be6aac8d1f89ec21ee261bb7a0ccfc2b29122ae9
Justin <justin82049@gmail.com>
2021-01-06 19:21:31 -0500

    added constructor

--- Node.java
+++ Node.java
@@ -1,6 +1,9 @@
 public class Node{
  private String data;
  private Node next,prev;
+ public Node(String value){
+   data = value;
+ }
  //write get/set methods for all three instance variables.
  public String getData(){
    Return data;
