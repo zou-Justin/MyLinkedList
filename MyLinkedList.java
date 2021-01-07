f2b9f6a3e9eac085021731d326c03477f5fe0eb5
Justin <justin82049@gmail.com>
2021-01-06 20:25:03 -0500

    added the constructor

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,7 +1,9 @@
 public class MyLinkedList{
  private int size;
  private Node start,end;  
- public MyLinkedList(){/*create a constructor*/}
+ public MyLinkedList(){
+   size = 0;
+ }
  public int size();
  public boolean add(String value);
  public void add(int index, String value);
