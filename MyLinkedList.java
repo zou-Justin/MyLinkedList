1dcccbb61fc4188fcd55c9b8377357e39ca8de43
Justin <justin82049@gmail.com>
2021-01-07 13:16:30 -0500

    did get() method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,29 +2,34 @@ public class MyLinkedList{
  private int size;
  private Node start,end;  
  public MyLinkedList(){
-   size = 0; 
+   size = 0;
  }
  public int size(){
    return size;
  }
  public boolean add(String value){
    size++;
-   end = value;
+   end = new Node(value);
+   return true;
  }
  public void add(int index, String value){
-  if (size == 0){
-    add(value);
+   // Node temp = helperIndexMethod(index);
+   // Node temp2 = temp.getNext();
+   // temp = temp.setNext().setData(value);
+   // temp2.getNext().setPrev().setData(value);
   }
- }
+ 
  private Node helperIndexMethod(int index){
    Node term = start;
-   for (int i =0; i < index;i++){
-     term = Start.getNext();
+   for (int i = 1; i < index;i++){
+     term = start.getNext();
    }
    return term;
  }
- public String get(int index);
- public String set(int index, String value);
- public String toString();
+  public String get(int index){
+    return helperIndexMethod(index).getData();
+  }
+  //public String set(int index, String value);
+ // public String toString();
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
