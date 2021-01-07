274d17ddb13c93ab08947ca15001b66fa8edf141
Justin <justin82049@gmail.com>
2021-01-06 23:19:00 -0500

    added a helper method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,13 +2,27 @@ public class MyLinkedList{
  private int size;
  private Node start,end;  
  public MyLinkedList(){
-   size = 0;
+   size = 0; 
  }
  public int size(){
    return size;
- };
- public boolean add(String value);
- public void add(int index, String value);
+ }
+ public boolean add(String value){
+   size++;
+   end = value;
+ }
+ public void add(int index, String value){
+  if (size == 0){
+    add(value);
+  }
+ }
+ private Node helperIndexMethod(int index){
+   Node term = start;
+   for (int i =0; i < index;i++){
+     term = Start.getNext();
+   }
+   return term;
+ }
  public String get(int index);
  public String set(int index, String value);
  public String toString();
