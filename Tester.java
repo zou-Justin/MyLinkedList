699cf53ca76297a6e0e04844c93e8e5a238d6d12
Justin <justin82049@gmail.com>
2021-01-10 22:10:13 -0500

    forgot to update my own testing tester

--- Tester.java
+++ Tester.java
@@ -2,6 +2,15 @@ public class Tester{
   public static void main(String[]args){
     MyLinkedList words = new MyLinkedList();
     words.add("hi");
+    words.add("i");
+    words.add("2hi");
+    words.add("h3i");
+    words.add("hi4");
+    words.add("hi21");
+    words.add("hi32");
+    words.add(0,"bob");
+    //words.set(0,"bob");
+    //System.out.print(words.get(1));
     System.out.print(words);
 }
 }
